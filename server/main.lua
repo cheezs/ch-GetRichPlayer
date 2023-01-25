60836b50dd54af389e32bb1c60a647a151f4b6a0
cheezs <74582128+cheezs@users.noreply.github.com>
2023-01-25 11:27:47 -0600

    Updated main.lua
    This allows for the message to send every created character as opposed to previously only sending 1 character per FiveM license.

--- server/main.lua
+++ server/main.lua
@@ -40,7 +40,7 @@ end, 'admin')
 --Event for TOP RICHEST ONLY
 RegisterNetEvent("mb-GetRichPlayer:server:getTopPlayerMoney", function(type)
     if (type == "full") then
-        local topRichestPlayers = MySQL.Sync.fetchAll("SELECT `name`, `money`, `citizenid`, `license`, JSON_VALUE(money, '$.cash') + JSON_VALUE(money, '$.bank') AS `total_money` FROM `players` GROUP BY `name` ORDER BY `total_money` DESC LIMIT ?", {Config.OnlyTopRichest.top})
+        local topRichestPlayers = MySQL.Sync.fetchAll("SELECT `name`, `money`, `citizenid`, `license`, JSON_VALUE(money, '$.cash') + JSON_VALUE(money, '$.bank') AS `total_money` FROM `players` GROUP BY `citizenid` ORDER BY `total_money` DESC LIMIT ?", {Config.OnlyTopRichest.top})
         local resultWithLicense = ''
 
         for _, v in pairs(topRichestPlayers) do
@@ -49,7 +49,7 @@ RegisterNetEvent("mb-GetRichPlayer:server:getTopPlayerMoney", function(type)
 
         sendToDiscord(resultWithLicense) --Send log to discord
     elseif (type == "standard") then
-        local topRichestPlayers = MySQL.Sync.fetchAll("SELECT `name`, `money`, `citizenid`, JSON_VALUE(money, '$.cash') + JSON_VALUE(money, '$.bank') AS `total_money` FROM `players` GROUP BY `name` ORDER BY `total_money` DESC LIMIT ?", {Config.OnlyTopRichest.top})
+        local topRichestPlayers = MySQL.Sync.fetchAll("SELECT `name`, `money`, `citizenid`, JSON_VALUE(money, '$.cash') + JSON_VALUE(money, '$.bank') AS `total_money` FROM `players` GROUP BY `citizenid` ORDER BY `total_money` DESC LIMIT ?", {Config.OnlyTopRichest.top})
         local resultWithoutLicense = ''
 
         for _, v in pairs(topRichestPlayers) do
@@ -58,7 +58,39 @@ RegisterNetEvent("mb-GetRichPlayer:server:getTopPlayerMoney", function(type)
 
         sendToDiscord(resultWithoutLicense) --Send log to discord
     else
-        local topRichestPlayers = MySQL.Sync.fetchAll("SELECT `name`, `money`, `citizenid`, JSON_VALUE(money, '$.cash') + JSON_VALUE(money, '$.bank') AS `total_money` FROM `players` GROUP BY `name` ORDER BY `total_money` DESC LIMIT ?", {Config.OnlyTopRichest.top})
+        local topRichestPlayers = MySQL.Sync.fetchAll("SELECT `name`, `money`, `citizenid`, JSON_VALUE(money, '$.cash') + JSON_VALUE(money, '$.bank') AS `total_money` FROM `players` GROUP BY `citizenid` ORDER BY `total_money` DESC LIMIT ?", {Config.OnlyTopRichest.top})
+        local shortMsg = ''
+
+        for _, v in pairs(topRichestPlayers) do
+            shortMsg = shortMsg .. "`" .. _ .. "." .. Lang:t("message.short_top_message", {name = topRichestPlayers[_]["name"], citizenid = topRichestPlayers[_]["citizenid"], totalMoney = topRichestPlayers[_]["total_money"]})
+        end
+
+        sendToDiscord(shortMsg) --Send log to discord
+    end
+end)
+
+--Event for ALL PLAYER
+RegisterNetEvent("mb-GetRichPlayer:server:getAllPlayerMoney", function(type)
+    if (type == "full") then
+        local topRichestPlayers = MySQL.Sync.fetchAll("SELECT `name`, `money`, `citizenid`, `license`, JSON_VALUE(money, '$.cash') + JSON_VALUE(money, '$.bank') AS `total_money` FROM `players` GROUP BY `citizenid` ORDER BY `total_money` DESC")
+        local resultWithLicense = ''
+
+        for _, v in pairs(topRichestPlayers) do
+            resultWithLicense = resultWithLicense .. "`" .. _ .. "." .. Lang:t("message.top_with_license", {name = topRichestPlayers[_]["name"], citizenid = topRichestPlayers[_]["citizenid"], license = topRichestPlayers[_]["license"], money = topRichestPlayers[_]["money"], totalMoney = topRichestPlayers[_]["total_money"]})
+        end
+
+        sendToDiscord(resultWithLicense) --Send log to discord
+    elseif (type == "standard") then
+        local topRichestPlayers = MySQL.Sync.fetchAll("SELECT `name`, `money`, `citizenid`, JSON_VALUE(money, '$.cash') + JSON_VALUE(money, '$.bank') AS `total_money` FROM `players` GROUP BY `citizenid` ORDER BY `total_money` DESC")
+        local resultWithoutLicense = ''
+
+        for _, v in pairs(topRichestPlayers) do
+            resultWithoutLicense = resultWithoutLicense .. "`" .. _ .. "." .. Lang:t("message.top_without_license", {name = topRichestPlayers[_]["name"], citizenid = topRichestPlayers[_]["citizenid"], money = topRichestPlayers[_]["money"], totalMoney = topRichestPlayers[_]["total_money"]})
+        end
+
+        sendToDiscord(resultWithoutLicense) --Send log to discord
+    else
+        local topRichestPlayers = MySQL.Sync.fetchAll("SELECT `name`, `money`, `citizenid`, JSON_VALUE(money, '$.cash') + JSON_VALUE(money, '$.bank') AS `total_money` FROM `players` GROUP BY `citizenid` ORDER BY `total_money` DESC")
         local shortMsg = ''
 
         for _, v in pairs(topRichestPlayers) do
